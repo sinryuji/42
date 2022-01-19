735031e111783f77dd745820408226aa246f4ef2
sinryuji <siler2474@gmail.com>
2022-01-20 02:03:11 +0900

    C07 ing

---
+++ C06/ex02/ft_rev_params.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyeongki <hyeongki@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/19 17:02:26 by hyeongki          #+#    #+#             */
+/*   Updated: 2022/01/19 17:05:58 by hyeongki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	i = ac - 1;
+	while (i > 0)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			write(1, &av[i][j], 1);
+			j++;
+		}
+		i--;
+		write(1, "\n", 1);
+	}
+}
---
+++ C07/ex02/ft_ultimate_range.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyeongki <hyeongki@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/19 20:18:54 by hyeongki          #+#    #+#             */
+/*   Updated: 2022/01/19 20:50:55 by hyeongki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int ft_ultimate_range(int **range, int min, int max)
+{
+	int	i;
+	int	*arr;
+
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	arr = malloc(sizeof(int) * (max - min));
+	if (!arr)
+	{
+		*range = NULL;
+		return (-1);
+	}
+	i = 0;
+	while (min < max)
+	{
+		arr[i] = min;
+		i++;
+		min++;
+	}
+	*range = arr;
+	return (max - min);
+}
