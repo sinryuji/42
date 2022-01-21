d408914b8631a23993d44ab842d6144a8b591073
siryuji <siler2474@gmail.com>
2022-01-21 18:47:57 +0900

    pdf add

--- C07/ex05/ft_split.c
+++ C07/ex05/ft_split.c
@@ -12,5 +12,63 @@
 
 #include <stdlib.h>
 
-char **ft_split(char *str, char *charset)
+char	*ft_strdup(char *src)
+{
+	int		len;
+	char	*result;
+	int		i;
 
+	len = 0;
+	while (src[len])
+		len++;
+	result = malloc(sizeof(char) * (len + 1));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		result[i] = src[i];
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+int	sep_check(char c, char *charset)
+{
+	int	i;
+
+	i = 0;
+	while (*charset)
+	{
+		if (c == *charset)
+			return (1);
+		charset++;
+	}
+	return (0);
+}
+
+int	get_word_cnt(char *str, char *charset)
+{
+	int	i;
+	int cnt;
+	
+	i = 1;
+	cnt = 0;
+	if (!sep_check(str[0], charset))
+		cnt++;
+	while (str[i])
+	{
+		if (sep_ckeck(str[i - 1], charset) && !sep_check(str[i]))
+			cnt++;
+		i++;
+	}
+	return (cnt);
+}
+
+char **ft_split(char *str, char *charset)
+{
+	char	**result;
+	
+			result = malloc(
+}
