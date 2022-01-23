56f04127e4d39d77e5b8fe3b061dbd5462d59655
sinryuji <siler2474@gmail.com>
2022-01-24 00:40:46 +0900

    C09 ing

---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyeongki <hyeongki@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/23 17:15:50 by hyeongki          #+#    #+#             */
+/*   Updated: 2022/01/23 17:49:07 by hyeongki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS 0
+# define EVEN(nbr) nbr % 2 == 0
+typedef int	t_bool;
+
+#endif
