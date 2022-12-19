56f70af8ee32569a1b8e99d695230c1bec0499cb
sinryuji <siler2474@gmail.com>
2022-12-20 02:07:01 +0900

    feat: my_microsh.c parsing

--- 04/microshell/my_microsh.c
+++ 04/microshell/my_microsh.c
@@ -1,25 +1,29 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
-#define STDIN 0
-#define STDOUT 1
-#define STDERR 2
+#define STD_IN 0
+#define STD_OUT 1
+#define STD_ERR 2
 
 #define TYPE_END 0
 #define TYPE_PIPE 1
 #define TYPE_BREAK 2
 
-typedef struct	s_cmd
+typedef struct s_cmd
 {
-	char			**cmd;
+	char 			**argv;
+	int				type;
+	int				size;
 	struct s_cmd	*next;
 }	t_cmd;
 
-int	ft_strlen(char *str)
+int ft_strlen(char *str)
 {
-	int	i = 0;
+	int	i;
 
+	i = 0;
 	if (!str)
 		return (0);
 	while (str[i])
@@ -27,43 +31,109 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
-void	err_fatal()	
+void	err_fatal()
 {
-	write(STDERR, "error: fatal\n", ft_strlen("error: fatal\n"));
+	write(STD_ERR, "error: fatal\n", ft_strlen("error_fatal\n"));
 	exit(EXIT_FAILURE);
+}
+
+char *ft_strdup(char *str)
+{
+	int i = 0;
+	char	*new;
+
+	if (!str)
+		return (NULL);
+	if (!(new = (char *)malloc(sizeof(char) * (ft_strlen(str) + 1))))
+		err_fatal();
+	while (str[i])
+	{
+		new[i] = str[i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
 }
 
 int	argv_size(char **argv)
 {
 	int	i = 0;
-
 	while (argv[i] && strcmp(argv[i], "|") != 0 && strcmp(argv[i], ";") != 0)
 		i++;
 	return (i);
 }
 
+t_cmd *get_last_node(t_cmd *cmd)
+{
+	if (!cmd)
+		return (NULL);
+	while (cmd->next)
+		cmd = cmd->next;
+	return (cmd);
+}
+
+int	get_type(char *argv)
+{
+	if (argv == NULL)
+		return (TYPE_END);
+	if (strcmp(argv, "|") == 0)
+		return (TYPE_PIPE);
+	else if (strcmp(argv, ";") == 0)
+		return (TYPE_BREAK);
+	return (0);
+}
+
 int	parse(t_cmd **cmd, char **argv)
 {
 	int	size = argv_size(argv);
-	t_cmd	*new;
-	if (!(new = malloc(sizeof(t_cmd))))
+	t_cmd *new;
+	int	i = 0;
+
+	if (!(new = (t_cmd *)malloc(sizeof(t_cmd))))
 		err_fatal();
+	if (!(new->argv = (char **)malloc(sizeof(char *) * (size + 1))))
+		err_fatal();
+	while (i < size)
+	{
+		new->argv[i] = ft_strdup(argv[i]);
+		i++;		
+	}
+	new->argv[i] = NULL;
+	new->size = size;
+	new->type = get_type(argv[size]);
+	if (*cmd == NULL)
+		*cmd = new;
+	else
+		get_last_node(*cmd)->next = new;
+	if (new->type == TYPE_PIPE || new->type == TYPE_BREAK)
+		size++;
+	return (size);
 }
 
-int	main(int argc, char **argv, char **envp)
+int main(int argc, char **argv, char **envp)
 {
 	t_cmd	*cmd;
-	int		i;
-	
-	if (argc <= 1)
-		return (0);
-	i = 0;
+	int	i;
+
+	if (argc < 1)
+		return (EXIT_SUCCESS);
 	cmd = NULL;
+	i = 1;
 	while (argv[i])
 	{
 		i += parse(&cmd, &argv[i]);
-		i++;
 	}
-
-	return (0);
+	while (cmd)
+	{
+		int j = 0;
+		while (cmd->argv[j])
+		{
+			printf ("%s ", cmd->argv[j]);
+			j++;
+		}
+		printf("%d", cmd->type);
+		cmd = cmd->next;
+		printf("\n");
+	}
+	return (EXIT_SUCCESS);
 }
